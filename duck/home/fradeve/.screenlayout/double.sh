eb84db8b17601ab306978251464a4195f1daffc1
fradeve <fradeve@inventati.org>
2016-07-04 00:07:26 +0100

    updated i3 / xinitrc/ xbindkeysrc / screen scripts for Duck

--- duck/home/fradeve/.screenlayout/double.sh
+++ duck/home/fradeve/.screenlayout/double.sh
@@ -5,3 +5,4 @@ xrandr --output VIRTUAL1 --off \
     --output HDMI2 --off \
     --output HDMI1 --off \
     --output DP2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+feh --bg-fill /home/fradeve/.wallpaper.jpg
