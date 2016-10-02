22e2af8ebb447a9f0ef65408b6fdd28e5f92e5e1
fradeve <fradeve@inventati.org>
2016-10-02 17:24:21 +0100

    removed emmett

--- emmett/home/fradeve/.bin/backup_mysql.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm $HOME/.bin/mysqldump_all_databases.sql
-/usr/bin/mysqldump --defaults-extra-file=/home/fradeve/.keys/mysqldump/my.cnf --all-databases > $HOME/.bin/mysqldump_all_databases.sql
-/bin/chmod 600 $HOME/.bin/mysqldump_all_databases.sql
