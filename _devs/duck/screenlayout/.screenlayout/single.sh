1859eab7a7dcd257963742c3c0af8dacca2e7b8d
fradeve <fradeve@inventati.org>
2017-02-07 16:36:38 +0000

    fixed screen resolution, taskwarrior reports, backups

--- _devs/duck/screenlayout/.screenlayout/single.sh
+++ _devs/duck/screenlayout/.screenlayout/single.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 xrandr --newmode "1680x1050_60.00"  146.25  1680 1784 1960 2240  1050 1053 1059 1089 -hsync +vsync
-xrandr --addmode eDP-1 "1680x1050_60.00"
+xrandr --addmode eDP1 "1680x1050_60.00"
 xrandr --output VIRTUAL1 --off \
-    --output eDP-1 --mode "1680x1050_60.00" --pos 0x0 --rotate normal \
+    --output eDP1 --mode "1680x1050_60.00" --pos 0x0 --rotate normal \
     --output DP1 --off \
     --output HDMI2 --off \
     --output HDMI1 --off \
